141ba6e1b6b15ede0e20ca9491d72f395a6fc8d3
sakthiofficial <sakthiroky123@gmail.com>
2023-01-18 18:50:13 +0530

    sample

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Urlshortner />} />
-          <Route path="/:url" element={<Website />}>
+          <Route path="/hoooc" element={<Website />}>
 
 
           </Route>
