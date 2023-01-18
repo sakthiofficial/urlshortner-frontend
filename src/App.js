1d3fc730f1af997204c35430992b573d91d20963
sakthiofficial <sakthiroky123@gmail.com>
2023-01-18 19:14:24 +0530

    navigate fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Urlshortner } from './Urlshortner';
 import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
-import { Website } from './Website';
+import { Sample, Website } from './Website';
 import { useEffect, useState } from 'react';
 function App() {
   return (
@@ -22,21 +22,5 @@ function App() {
     </div>
   );
 }
-{/* */ }
-function Sample() {
-  const { url } = useParams();
-  const [longurl, setlongurl] = useState("");
-  const getUrl = async () => {
-    const dt = await fetch(`http://localhost:4000/${url}`);
-    const val = await dt.json()
-    setlongurl(val.longurl)
-  };
-  useEffect(() => getUrl, []);
-  window.location.href = longurl;
-  console.log(longurl);
-  console.log(url);
-  return (
-    <h1>Iam Sample</h1>
-  )
-}
+
 export default App;
--- src/Website.js
+++ src/Website.js
@@ -2,18 +2,17 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from 'react';
 import HourglassEmptyIcon from '@mui/icons-material/HourglassEmpty';
 
-export function Website() {
+export function Sample() {
     const { url } = useParams();
     const [longurl, setlongurl] = useState("");
     const getUrl = async () => {
         const dt = await fetch(`http://localhost:4000/${url}`);
         const val = await dt.json()
-        setlongurl(val.longurl)
+        window.location.href = val.longurl;
+
     };
     useEffect(() => getUrl, []);
-    window.location.href = longurl;
-    console.log(longurl);
-    console.log(url);
+
     return (
 
         <div className="loding">
